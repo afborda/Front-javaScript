f21fe0016571a9296cfd0794c54550703443c9d7
Abner Fonseca <abner.borda@gmail.com>
2020-07-20 14:51:58 -0300

    Faltando a segunda parte

---
+++ js/maskInputs.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#cpfUser").mask("000.000.000-00", { reverse: true });
+});
+
+$(document).ready(function () {
+  $("#phoneUser").mask("(00) 0-0000-0000");
+});
+$(document).ready(function () {
+  $("#cepUser").mask("000000000", { reverse: true });
+});
